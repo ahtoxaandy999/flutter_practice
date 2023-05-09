6f6fdb2e5963a63fe9ebdc71ba2e16c7651895cf
Anton Tolkanets <tolkanec96@gmail.com>
2023-05-09 18:22:14 +0200

    Remove router

--- lib/domain/entities/ip_settings.dart
+++ lib/domain/entities/ip_settings.dart
@@ -3,30 +3,25 @@ import 'package:equatable/equatable.dart';
 class IpSettings extends Equatable {
   final String ipAddress;
   final String subnetMask;
-  final String router;
 
   const IpSettings({
     required this.ipAddress,
     required this.subnetMask,
-    required this.router,
   });
 
   IpSettings copyWith({
     String? ipAddress,
     String? subnetMask,
-    String? router,
   }) {
     return IpSettings(
       ipAddress: ipAddress ?? this.ipAddress,
       subnetMask: subnetMask ?? this.subnetMask,
-      router: router ?? this.router,
     );
   }
 
   @override
-  List<Object?> get props => [ipAddress, subnetMask, router];
+  List<Object?> get props => [ipAddress, subnetMask];
 
   @override
   bool get stringify => true;
 }
-
